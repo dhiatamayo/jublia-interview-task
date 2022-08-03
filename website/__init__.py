11f25905454295bd772379a43d90c4e6b193d369
Dhiatamayo Fadhlika <dhiatamayo.fadhlika@mobileum.com>
2022-08-04 02:49:29 +0700

    remove scheduler initialization

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_apscheduler import APScheduler
-from apscheduler.schedulers.background import BackgroundScheduler
 from os import path
 
 db = SQLAlchemy()
 DB_NAME = 'emails.db'
-
-scheduler = BackgroundScheduler()
 
 def create_app():
     app = Flask(__name__)
